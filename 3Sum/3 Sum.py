8f6fea465c8d61aba1b7eb3bd92e4c923dd5c42d
AyonDebnath <ayonnath.31@gmail.com>
2024-08-21 21:31:19 -0230

    3 Sum solved

--- 3Sum/3 Sum.py
+++ 3Sum/3 Sum.py
@@ -8,14 +8,7 @@ class Solution:
             # triplet = [a, b, c]
             a = nums[i]
 
-            if duplicationAlertFlag:
-                duplicationAlertFlag = 0
-                continue
-
-            if len(nums) > 0 and i < len(nums) -1 and nums[i] == nums[i+1]:
-                duplicationAlertFlag = 1
-
-            if i > 0 and nums[i] == nums[i-1] and nums[i-1] == nums[i-2]:
+            if i > 0 and nums[i] == nums[i-1] :
                 continue
 
             leftPointer = i + 1
@@ -30,14 +23,20 @@ class Solution:
                     rightPointer -= 1
                 elif sum == target:
                     triplets.append([nums[i], nums[leftPointer], nums[rightPointer]])
-                    if nums[i] == nums[leftPointer]:
-                        duplicationAlertFlag = 0
-                    break
+
+                    originalLeftPointer = leftPointer
+                    leftPointer += 1
+                    while(leftPointer < len(nums) and nums[originalLeftPointer] == nums[leftPointer]):
+                        leftPointer += 1
+                    originalRightPointer = 1
+                    rightPointer -= 1
+                    while (rightPointer > 0 and nums[originalRightPointer] == nums[rightPointer]):
+                        rightPointer -= 1
         return triplets
 
 
 
 sol = Solution()
-# print(sol.threeSum([-4, -1,-1,0,1,2]))
-# print(sol.threeSum([0,0,0,0]))
+print(sol.threeSum([-4, -1,-1,0,1,2]))
+print(sol.threeSum([0,0,0,0]))
 print(sol.threeSum([-2,0,1,1,2]))
