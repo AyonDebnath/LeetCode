48adb51d60771d5456a36cc2a7ec8885420253ba
AyonDebnath <ayonnath.31@gmail.com>
2024-09-09 00:09:33 -0230

    Permutation in String

--- Permutation in String/Permutation in String.py
+++ Permutation in String/Permutation in String.py
@@ -1,47 +1,54 @@
-import copy
-
 
 class Solution:
 
-    def getFrequency(self, s):
-        dict = {}
-        for s in s:
-            dict[s] = 1 + dict.get(s, 0)
-
-        return dict
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        freqS1 = self.getFrequency(s1)
-        s1Cpy = list(s1)
-        started = False
-
-        i = 0
-        while i < len(s2):
-            if started == True and s2[i] not in s1Cpy:
-                if s2[i] in s1:
-                    i = startIndex + 1
-                    s1Cpy = list(s1)[i:]
-                else:
-                    return False
-
-            if s2[i] in s1Cpy:
-                if not started:
-                    startIndex = i
-                    started = True
-
-                s1Cpy.remove(s2[i])
-                if s1Cpy == []:
-                    return True
-            i+=1
-
-        if s1Cpy == []:
-            return True
-        else:
+        if len(s1) > len(s2):
             return False
 
-sol = Solution()
-# print(sol.checkInclusion("adc", "dcda"))
+        s1Count, s2Count = [0] * 26, [0] * 26
 
-print(sol.checkInclusion("hello", "ooolleoooleh"))
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+
+        for i in range(26):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+
+        l = 0
+        for r in range(len(s1), len(s2), 1):
+            if matches == 26:
+                return True
+
+            index = ord(s2[r]) - ord('a')
+            if s2Count[index] == s1Count[index]:
+                matches -= 1
+            s2Count[index] += 1
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+
+            index = ord(s2[l]) - ord('a')
+
+            if s2Count[index] == s1Count[index]:
+                matches -= 1
+            s2Count[index] -= 1
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+
+            l += 1
+
+        return matches == 26
+
+
+
+
+sol = Solution()
+print(sol.checkInclusion("adc", "dcda"))
+print(sol.checkInclusion("ab", "eidbo"))
+
+# print(sol.checkInclusion("hello", "ooolleoooleh"))
 
 
 
