145c767659573e2ee006a383f9afb5656f735175
AyonDebnath <ayonnath.31@gmail.com>
2024-09-21 22:27:27 -0230

    reorder list in progress

--- Reorder List/reorder list.py
+++ Reorder List/reorder list.py
@@ -1,6 +1,10 @@
 # Definition for singly-linked list.
 import copy
+'''
 
+TIME LIMIT EXCEEDED
+
+'''
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -12,6 +16,9 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
 
+        if head.next is None:
+            return
+
         pointer = copy.deepcopy(head)
         nextNode = copy.deepcopy(head.next)
         pointer.next = None
@@ -22,30 +29,30 @@ class Solution:
             nextNode.next = pointer
             pointer = nextNode
             nextNode = temp
+        length += 1
         headRvrsd = pointer
 
 
         pointer1 = head
         pointer2 = headRvrsd
-        for i in range(length//2 + 1):
+        for i in range(length//2):
             temp = pointer1.next
+            tail = pointer1.next
             pointer1.next = pointer2
             pointer1 = temp
 
-
-            if i == length//2 + 1:
-                continue
+            if length %2 == 0 and i == length//2 -1 :
+                tail = pointer2
+                break
 
             temp = pointer2.next
             pointer2.next = pointer1
             pointer2 = temp
 
 
+        tail.next = None
         while head is not None:
             print(head.val)
             head = head.next
-
-
-
 sol = Solution()
-print(sol.reorderList(ListNode(1, ListNode(2, ListNode(3)))))
+print(sol.reorderList(ListNode(1, ListNode(2, ListNode(3, ListNode(4))))))
