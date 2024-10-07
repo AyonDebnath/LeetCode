a51b5005d7b87b4f8bbb31880a305fdd82298d07
Ayon Debnath <ayonnath.31@gmail.com>
2024-10-06 22:56:06 -0230

    Structure created for LRU Cache

---
+++ LRU Cache/LRU Cache.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self):
+        self.val = None
+        self.prev = None
+        self.next = None
+
+
+class Solution:
+    def remove(self, node):
+        # the last node from the linked list
+        pass
+
+    def insert(self, node):
+        # insert the node at the beginning of the linked list
+        pass
+
+    def findDuplicate(self, nums: [int]) -> int:
+        pass
