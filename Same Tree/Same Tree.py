1c86bef92c0e8c68cc3e2cc8b9b657ff0573b1a9
Ayon Debnath <ayonnath.31@gmail.com>
2024-10-14 18:10:36 -0230

    Balanced Binary Tree - more efficient

---
+++ Same Tree/Same Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p, q) -> bool:
+        if p is None or q is None:
+            if p is not None or q is not None:
+                return False
+            return True
+
+        isEqual = p.val == q.val
+        if not isEqual:
+            return False
+
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+
+        return left and right
+
