5ec31e17255d5df367cadc4c46596b7d3023a748
Ayon Debnath <ayonnath.31@gmail.com>
2024-11-13 21:30:04 -0330

    Kth Smallest Element in Bst

--- Kth Smallest Element in Bst/kth Smalest Element in Bst.py
+++ Kth Smallest Element in Bst/kth Smalest Element in Bst.py
@@ -1,25 +1,28 @@
 # Definition for a binary tree node.
-from socket import send_fds
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
     def __init__(self):
         self.lst = []
 
     def populate(self, root):
+        if root is None:
+            return
         if root.left is None:
             self.lst.append(root)
             self.populate(root.right)
-        self.populate(root.left)
+        else:
+            self.populate(root.left)
+            self.lst.append(root)
+            if root.right is not None:
+                self.populate(root.right)
 
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
         self.populate(root)
-        return self.lst[k-1].val
+        print(self.lst)
+        return self.lst[k - 1].val
