1765cf1b2840067c758b1e461272229ee1ac419c
Ayon Debnath <ayonnath.31@gmail.com>
2024-11-24 10:57:59 -0330

    Consttruct Binary Tree

--- Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -11,8 +11,11 @@ class TreeNode:
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if preorder == [] :
-            return TreeNode()
+            return None
 
-        root = preorder[0]
+        root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
-        # root.left = self.buildTree(preorder[1:], in)
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
