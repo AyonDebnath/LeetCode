c9bda8899ec45b2a27fbabb08a14d2f4e71b7d8e
Ayon Debnath <ayonnath.31@gmail.com>
2025-01-08 21:39:29 -0330

    Task Scheduler (in progress)

--- Task Scheduler/Task Scheduler.py
+++ Task Scheduler/Task Scheduler.py
@@ -4,7 +4,7 @@ from typing import List
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        time = 0
+        time = 1
 
         distinctCount = [0]*26
         for task in tasks:
@@ -13,24 +13,22 @@ class Solution:
 
         waitingQueue = [] # [[time, -remainingCharacterCount]]
         result = []
-        while len(distinctCount) != 0 or len(waitingQueue) != 0:
+        while sum(distinctCount) < 0 or len(waitingQueue) != 0:
             curr = None
-            if len(waitingQueue) != 0 and waitingQueue[0][0] == time:
+            if len(waitingQueue) > 0 and waitingQueue[0][0] == time:
                 curr = -waitingQueue.pop(0)[1]
                 result.append('.')
                 curr -= 1
-
-                heapq.heappush(distinctCount, -curr)
-            else:
+                if curr != 0:
+                    waitingQueue.append([time + n + 1, -curr])
+            elif sum(distinctCount) < 0:
                 curr = -heapq.heappop(distinctCount)
-                if curr == 0 and len(waitingQueue) != 0:
-                    result.append('.')
-
-                if curr != 0 :
-                    result.append('.')
-                    curr -=1
-                    if curr != 0:
-                        waitingQueue.append([time+n, -curr])
+                result.append('.')
+                curr -=1
+                if curr != 0:
+                    waitingQueue.append([time+n+1, -curr])
+            elif sum(distinctCount) == 0:
+                result.append('.')
 
             time += 1
 
