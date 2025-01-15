ed12308cfe75ea10f24485295886509abd717e4c
Ayon Debnath <ayonnath.31@gmail.com>
2025-01-14 21:20:35 -0330

    setup insert interval (incorrect)

--- Insert Interval/Insert Interval.py
+++ Insert Interval/Insert Interval.py
@@ -3,11 +3,20 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        output = []
+        started = False
         for interval in intervals:
             start = interval[0]
             end = interval[1]
 
-            if start < newInterval[0] and end > newInterval[0]:
+            temp = []
+
+            if start < newInterval[0] < end and not started:
+                temp.append(start)
+                started = True
+            elif start < newInterval[1] < end and not started:
+                pass
+            elif newInterval[1] < start and not started:
                 pass
             else:
-                pass
+                output.append(interval)
