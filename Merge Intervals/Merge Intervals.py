cfafcbf2657cb6147d148287daf7ee20fab4d5b8
Ayon Debnath <ayonnath.31@gmail.com>
2025-01-17 23:47:07 -0330

    Merge Intervals in progress

--- Merge Intervals/Merge Intervals.py
+++ Merge Intervals/Merge Intervals.py
@@ -4,34 +4,39 @@ from typing import List
 class Solution:
 
     def mergeOverlaps(self, range1:List[int], range2:List[int]) -> List[int]:
-        pass
+        a = min(range1[0], range2[0])
+        b = max(range1[1], range2[1])
+        return [a, b]
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
-        intervals.sort()
-
         n = len(intervals)
-
         if n < 2:
             return intervals
 
+        intervals.sort()
         curr = intervals[0]
-        next = intervals[1]
+        nxt = intervals[1]
         i = 2
         res = []
 
         while i < n:
+            if curr[1] >= nxt[0]:
+                curr = self.mergeOverlaps(curr, nxt)
+                nxt = intervals[i]
+                i+=1
+            else:
+                res.append(curr)
+                curr = nxt
+                nxt = intervals[i]
 
-            # if curr and next overlaps
-            #   curr = new_interval
-            #   next = intervals[i]
-            #   i++
-            # else
-            #   res.append(curr)
-            #   curr = next
-            #   next = intervals[i]
+        if curr[1] >= nxt[0]:
+            curr = self.mergeOverlaps(curr, nxt)
+            res.append(curr)
 
-    # if curr and next overlaps:
-    #   curr = new_interval
-    #   res.append(curr)
+        if len(res) == 0:
+            return intervals
 
+        return res
+
+sol = Solution()
+print(sol.merge([[2,3],[5,5],[2,2],[3,4],[3,4]]))
