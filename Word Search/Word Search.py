6a969f6852e0349fdde429083eb18382b9adfb14
Ayon Debnath <ayonnath.31@gmail.com>
2025-11-09 15:53:45 -0330

    word search in progress

--- Word Search/Word Search.py
+++ Word Search/Word Search.py
@@ -3,12 +3,26 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if r >= ROWS or c >= COLS or (r, c) in path :
+                return False
+
+            if i == len(word):
+                return True
+
+            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
+
+            return res
 
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    # base case, search the boarders.
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if dfs(i, j, 0):
+                    return True
 
                 # If a match is not found check the next element in the board
                 j+=1
