9de5e2faaa2172481cee9072c50fc080d4d70ef5
Ayon Debnath <ayonnath.31@gmail.com>
2025-11-12 22:02:10 -0330

    word search

--- Word Search/Word Search.py
+++ Word Search/Word Search.py
@@ -7,23 +7,28 @@ class Solution:
         path = set()
 
         def dfs(r, c, i):
-            if r >= ROWS or c >= COLS or (r, c) in path :
+            if r >= ROWS or r < 0 or c >= COLS or c < 0 or (r, c) in path or word[i] != board[r][c]:
                 return False
 
-            if i == len(word):
+            if i == len(word)-1:
                 return True
 
+            path.add((r, c))
             res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
-
+            path.remove((r, c))
             return res
 
 
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if dfs(i, j, 0):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if dfs(row, col, 0):
                     return True
 
                 # If a match is not found check the next element in the board
-                j+=1
-            i+=1
+                col+=1
+            row+=1
+        return False
+
+sol = Solution()
+print(sol.exist([["C","A","A"],["A","A","A"],["B","C","D"]], "AAB"))
