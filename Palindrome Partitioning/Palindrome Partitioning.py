c1fcd8c503ec3946622edc1c0cfd7ee8fb1d8690
Ayon Debnath <ayonnath.31@gmail.com>
2025-11-21 19:58:52 -0330

    isPalindrome in progress

--- Palindrome Partitioning/Palindrome Partitioning.py
+++ Palindrome Partitioning/Palindrome Partitioning.py
@@ -13,6 +13,32 @@ class Solution:
         else:
             return False
 
+    def getSubstrings(self, s):
+        lst = []
+
+        def dfs(c, string):
+            if c in lst:
+                return
+            lst.append(c)
+            if len(string) == 0:
+                return
+
+            if len(string) == 1:
+                dfs(c + string[0], '')
+            else:
+                i2 = 1
+                while i2 < len(string) and string[i2-1] == string[i2]:
+                    i2 += 1
+                dfs(c + string[0], string[i2:])
+
+        s = list(s)
+        s.sort()
+        for i in range(len(s)):
+            dfs(s[i], s)
+        return lst
+
     def partition(self, s: str) -> List[List[str]]:
         pass
 
+sol = Solution()
+print(sol.getSubstrings('aba'))
